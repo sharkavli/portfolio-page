9701c84844bdb4e11edbb123b09a11864a2abcab
sharkavli <saidovavli@gmail.com>
2023-07-12 18:38:46 +0500

    games page added at 0.1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './components/Main/Main.js';
 import AboutMe from './components/AboutMe/AboutMe.js';
 import Projects from './components/Projects/Projects.js';
+import Games from './components/Games/Games.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="portfolio-page/" element={<Main />} />
           <Route path="/about-me" element={<AboutMe />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/games" element={<Games />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Games/Games.js
@@ -0,0 +1,17 @@
+import Header from '../Header';
+
+export default function Games() {
+  return (
+    <div className="games">
+      <Header />
+      <h1
+        style={{
+          margin: '0',
+          paddingTop: '100px',
+        }}
+      >
+        Пока здесь ничего нет, то скоро будет!
+      </h1>
+    </div>
+  );
+}
